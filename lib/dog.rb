3f85b260025ed7a55c8fa98878fc923ba32893dc
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-11 22:10:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,19 @@ class Dog
   
   attr_accessor :owner, :mood
   attr_reader :name
+  
+  @@all = []
 
 
   def initialize(name, ownerName)
     @name = name
     @owner = ownerName
     @mood = "nervous"
+    @@all << self
   end
     
+  def self.all
+    @@all
+  end
 
 end
